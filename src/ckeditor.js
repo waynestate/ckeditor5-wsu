905074245efde632d4b860224b0aeebc05f9cd50
Chris Pelzer <chrispelzer@gmail.com>
2023-12-01 19:42:14 -0500

    move app.js to src/ckeditor.js and build with ClassicEditor being exported so we can handle multiple instance with different configurations easily

---
+++ src/ckeditor.js
@@ -0,0 +1,93 @@
+// app.js
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';  // Importing the package.
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
+import {
+    Image,
+    ImageCaption,
+    ImageInsertViaUrl,
+    ImageStyle,
+    ImageToolbar,
+} from '@ckeditor/ckeditor5-image';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+
+
+export default class ClassicEditor extends ClassicEditorBase {}
+
+ClassicEditor.builtinPlugins = [
+    Alignment, 
+    Autoformat, 
+    BlockQuote,
+    Bold, 
+    Essentials, 
+    Heading, 
+    Image, 
+    ImageCaption, 
+    ImageInsertViaUrl,
+    ImageStyle, 
+    ImageToolbar, 
+    Indent, 
+    Italic, 
+    Link, 
+    List, 
+    MediaEmbed, 
+    Paragraph, 
+    PasteFromOffice, 
+    Table, 
+    TableToolbar, 
+    TextTransformation,
+];
+
+ClassicEditor.defaultConfig = {
+    toolbar: {
+        items: [
+            'alignment',  // Displaying the proper UI element in the toolbar.
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'outdent',
+            'indent',
+            '|',
+            'insertImage',
+            'blockQuote',
+            'insertTable',
+            'mediaEmbed',
+            'undo',
+            'redo'
+        ]
+    },
+    language: 'en',
+    image: {
+        toolbar: [
+            'imageTextAlternative',
+            'toggleImageCaption',
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells'
+        ]
+    },
+};
